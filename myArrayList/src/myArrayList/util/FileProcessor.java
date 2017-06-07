2b88eadaf3a2a891732a43b11e0d2d73670bd558
Paresh <pgiri1@binghamton.edu>
2017-06-07 01:16:03 -0400

    File procesor Updated

--- myArrayList/src/myArrayList/util/FileProcessor.java
+++ myArrayList/src/myArrayList/util/FileProcessor.java
@@ -1,12 +1,36 @@
 package myArrayList.util;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 public class FileProcessor {
 
-	public static String readLine(){
+	private static BufferedReader br;
+
+	public static String readLine(){ //return one line at a time
 		
 	    String str="";
+		try{
+	    File file =new File("input.txt");
+	    if(!file.exists()){
+	    	System.out.println("Please provide Input file");
+	    	}
+		
+	    FileReader fileReader = new FileReader(file);
+        br = new BufferedReader(fileReader);
+        while((str=br.readLine()) !=null) 
+        {
+        	return str; // change for each line
+        }
+        
+
+		}catch(IOException ie)
+		{
+			ie.printStackTrace();
+		}
 	    
-		return str;
+		return null;
 		
 	}
 }
